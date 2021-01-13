5b9247fac57cb49a11806338dcee3eeaf8b7e9dd
treav <write2treav@gmail.com>
2021-01-14 03:09:13 +0530

    Minor fix

--- lib/screens/login/cubit/login_cubit.dart
+++ lib/screens/login/cubit/login_cubit.dart
@@ -1,15 +1,12 @@
 import 'package:bloc/bloc.dart';
 import 'package:ecellapp/core/res/errors.dart';
 import 'package:ecellapp/core/res/strings.dart';
-import 'package:ecellapp/screens/login/login_repository.dart';
 import 'package:equatable/equatable.dart';
-import 'package:ecellapp/core/res/errors.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  final LoginRepository _loginRepo;
-  LoginCubit(this._loginRepo) : super(LoginInitial());
+  LoginCubit() : super(LoginInitial());
 
   Future<void> login(String email, String password) async {
     try {
