ca2486b70161f31ff7eed433812cec16ea68b4a8
treavvasu <write2treav@gmail.com>
2021-01-16 21:33:44 +0530

    LoginCubit (#23)

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:ecellapp/screens/signup/cubit/signup_cubit.dart';
-import 'package:ecellapp/screens/signup/signup.dart';
-import 'package:ecellapp/screens/signup/signup_repository.dart';
+import 'package:ecellapp/screens/login/cubit/login_cubit.dart';
+import 'package:ecellapp/screens/login/login.dart';
+import 'package:ecellapp/screens/login/login_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -14,8 +14,8 @@ class ECellApp extends StatelessWidget {
     return MaterialApp(
       title: "ECellApp",
       home: BlocProvider(
-        create: (context) => SignupCubit(FakeSignupRepository()),
-        child: SignupScreen(),
+        create: (context) => LoginCubit(FakeLoginRepository()),
+        child: LoginScreen(),
       ),
     );
   }
