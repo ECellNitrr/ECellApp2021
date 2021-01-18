c333222db40a7ee8b166da4e073fe425e603429e
Viren Khatri <werainkhatri.work@gmail.com>
2021-01-18 15:48:16 +0530

    formatting logger.dart

--- lib/core/utils/logger.dart
+++ lib/core/utils/logger.dart
@@ -12,24 +12,19 @@ class Log {
   Log._();
 
   /// Log a message of level {verbose}
-  static void v({@required String tag, @required String message}) =>
-      _logger.v("[$tag]: $message");
+  static void v({@required String tag, @required String message}) => _logger.v("[$tag]: $message");
 
   /// Log a message of level {debug}
-  static void d({@required String tag, @required String message}) =>
-      _logger.d("[$tag]: $message");
+  static void d({@required String tag, @required String message}) => _logger.d("[$tag]: $message");
 
   /// Log a message of level {info}
-  static void i({@required String tag, @required String message}) =>
-      _logger.i("[$tag]: $message");
+  static void i({@required String tag, @required String message}) => _logger.i("[$tag]: $message");
 
   /// Log a message of level {warning}
-  static void w({@required String tag, @required String message}) =>
-      _logger.w("[$tag]: $message");
+  static void w({@required String tag, @required String message}) => _logger.w("[$tag]: $message");
 
   /// Log a message of level {error}
-  static void e({@required String tag, @required String message}) =>
-      _logger.e("[$tag]: $message");
+  static void e({@required String tag, @required String message}) => _logger.e("[$tag]: $message");
 
   /// Log a message of level {severe}
   static void s({@required String tag, @required String message}) =>
