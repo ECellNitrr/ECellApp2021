a8f4483979106cc9072985eedb67db3014e07d3d
Hard-Coder05 <smishra1605@gmail.com>
2021-01-18 23:48:07 +0530

    feat:SignUp-API

--- lib/screens/signup/widgets/lastname_field.dart
+++ lib/screens/signup/widgets/lastname_field.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 
-class NameField extends StatelessWidget {
-  const NameField(this.controller);
+class LastNameField extends StatelessWidget {
+  const LastNameField(this.controller);
   final TextEditingController controller;
 
   @override
   Widget build(BuildContext context) {
     return TextFormField(
       controller: controller,
-      keyboardType: TextInputType.name,
+      keyboardType: TextInputType.text,
       decoration: InputDecoration(
         suffixText: '*',
         suffixStyle: TextStyle(color: Colors.red, fontSize: 20),
         border: OutlineInputBorder(),
-        labelText: "Full Name",
+        labelText: "Last Name",
       ),
     );
   }
