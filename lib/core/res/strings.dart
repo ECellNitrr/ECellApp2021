276283ea23fd327e444ee08d60e0f3800ccd600d
treav <write2treav@gmail.com>
2021-01-20 08:54:08 +0530

    updating with upstream

--- lib/core/res/strings.dart
+++ lib/core/res/strings.dart
@@ -15,18 +15,5 @@ class S {
   // error strings
   static const String networkException =
       "Request couldn't be completed. Make sure the device is online";
-<<<<<<< HEAD
-
-  // url
-  static const String url = "http://159.65.158.103:3000/";
-  static const String loginUrl = url + "/users/login/";
-
-  //SharedPref Keys
-  static const String emailKey = "LoginEmail";
-  static const String passwordKey = "PasswordKey";
-  static const String tokenKey = "AuthToken";
-  static const String auth = "Auth";
-=======
   static const String unknownException = "Something went wrong. Please try again later.";
->>>>>>> 24498855f808e32daec0066c8eb8260082483bd8
 }
