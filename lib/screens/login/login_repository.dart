6e880666f8c248936ddede669151aa0725d0f06b
treav <write2treav@gmail.com>
2021-01-22 10:17:54 +0530

    Update:login_repository

--- lib/screens/login/login_repository.dart
+++ lib/screens/login/login_repository.dart
@@ -48,7 +48,7 @@ class APILoginRepository implements LoginRepository {
     if (response.statusCode == 202) {
       try {
         String token = json.decode(response.body)[S.tokenKey];
-        Log.d(tag: tag, message: "Login Successful with token: " + token);
+        Log.i(tag: tag, message: "Login Successful with token: " + token);
         return token;
       } catch (e) {
         Log.e(tag: tag, message: "Error while decoding response json to get token: $e");
