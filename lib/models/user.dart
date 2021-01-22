fa07c932698dd42788dda375d61fab01fccd4db6
Siddharth Mishra <smishra1605@gmail.com>
2021-01-23 00:55:39 +0530

    Profile cubit (#43)

---
+++ lib/models/user.dart
@@ -0,0 +1,28 @@
+import 'package:ecellapp/core/res/strings.dart';
+import 'package:equatable/equatable.dart';
+
+class User extends Equatable {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final int phoneNumber;
+
+  User({
+    this.firstName,
+    this.lastName,
+    this.email,
+    this.phoneNumber,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      firstName: json[S.firstnameKey] as String,
+      lastName: json[S.lastnameKey] as String,
+      email: json[S.emailKey] as String,
+      phoneNumber: json[S.passwordKey] as int,
+    );
+  }
+
+  @override
+  List<Object> get props => [email];
+}
