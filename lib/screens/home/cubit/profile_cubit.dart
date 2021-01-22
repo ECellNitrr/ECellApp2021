98e0daaa00e376c2f877857d2d19ed8a6fa2578d
Siddharth Mishra <smishra1605@gmail.com>
2021-01-23 00:36:55 +0530

    Done
    Co-authored-by: Viren Manojkumar Khatri <werainkhatri.work@gmail.com>

--- lib/screens/home/cubit/profile_cubit.dart
+++ lib/screens/home/cubit/profile_cubit.dart
@@ -24,7 +24,7 @@ class ProfileCubit extends Cubit<ProfileState> {
     } on UnknownException {
       emit(ProfileError(S.unknownException));
     } catch (e) {
-      Log.s(tag: "FakeProfileRepository WeirdResponse", message: "Weird response message ->" + e);
+      Log.s(tag: "ProfileCubit", message: "Weird Error. message ->" + e.toString());
     }
   }
 }
