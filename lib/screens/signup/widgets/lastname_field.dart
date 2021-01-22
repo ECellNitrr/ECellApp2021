ff660ebc51d7500054a3b22bade529d82a009e30
DIVY ARPIT <61823356+divy-arpit@users.noreply.github.com>
2021-01-22 16:49:40 +0530

    Dedup common widgets (#38)

--- lib/screens/signup/widgets/lastname_field.dart
+++ lib/screens/signup/widgets/lastname_field.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class LastNameField extends StatelessWidget {
   const LastNameField(this.controller);
+
   final TextEditingController controller;
 
   @override
