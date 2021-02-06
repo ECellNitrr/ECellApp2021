af204eabed9debceb0d05ed24c2f6122bc537e3d
Divy Arpit <arpitdivy@gmail.com>
2021-02-06 17:31:14 +0530

    update

--- lib/screens/forgot_password/cubit/forgot_password_state.dart
+++ lib/screens/forgot_password/cubit/forgot_password_state.dart
@@ -4,12 +4,17 @@ part of 'forgot_password_cubit.dart';
 abstract class ForgotPasswordState {}
 
 class ForgotPasswordInitial extends ForgotPasswordState {}
-class ForgotPasswordLoading extends ForgotPasswordState{}
-class ForgotPasswordEnterOTP extends ForgotPasswordState{}
-class ForgotPasswordCorrectOTP extends ForgotPasswordState{}
-class ForgotPasswordWrongOTP extends ForgotPasswordState{}
-class ForgotPasswordError extends ForgotPasswordState{
+
+class ForgotPasswordLoading extends ForgotPasswordState {}
+
+class ForgotPasswordEnterOTP extends ForgotPasswordState {}
+
+class ForgotPasswordCorrectOTP extends ForgotPasswordState {}
+
+class ForgotPasswordWrongOTP extends ForgotPasswordState {}
+
+class ForgotPasswordError extends ForgotPasswordState {
   final String error;
+
   ForgotPasswordError(this.error);
 }
-
