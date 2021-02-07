60ef07b7e6d83da0fa36a7d15f71e7c792f334e0
treav <write2treav@gmail.com>
2021-02-07 13:47:04 +0530

    Minor fix

--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,7 @@ void main() async {
 class ECellApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setPreferredOrientations([
-      DeviceOrientation.portraitUp,
-    ]);
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: "ECellApp",
       theme: AppTheme.themeData(context),
