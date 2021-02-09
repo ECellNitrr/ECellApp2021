3c8a054c0eeb029087c052b8ac29d94ddc2469bf
Hard-Coder05 <smishra1605@gmail.com>
2021-02-09 10:54:20 +0530

    Reset original main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:ecellapp/screens/esummit/esummit.dart';
+import 'package:ecellapp/screens/login/cubit/login_cubit.dart';
+import 'package:ecellapp/screens/login/login.dart';
+import 'package:ecellapp/screens/login/login_repository.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart';
 import 'core/themes/app_theme.dart';
 import 'core/utils/injection.dart';
 
@@ -13,10 +16,14 @@ void main() async {
 class ECellApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
     return MaterialApp(
       title: "ECellApp",
       theme: AppTheme.themeData(context),
-      home: ESummit(),
+      home: BlocProvider(
+        create: (context) => LoginCubit(APILoginRepository()),
+        child: LoginScreen(),
+      ),
     );
   }
 }
