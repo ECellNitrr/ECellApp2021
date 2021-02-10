efc81f9fc0f82be44b8e2ecd29a79969cdeb2bdb
Hard-Coder05 <smishra1605@gmail.com>
2021-02-10 22:16:19 +0530

    Changes as asked in the review

--- lib/screens/home/tabs/profile/profile.dart
+++ lib/screens/home/tabs/profile/profile.dart
@@ -49,53 +49,71 @@ class _ProfileScreenState extends State<ProfileScreen> {
     double height = MediaQuery.of(context).size.height;
     double heightFactor = height / 1000;
     return DefaultTextStyle(
-        style: GoogleFonts.roboto().copyWith(color: C.primaryUnHighlightedColor),
-        child: Stack(children: [
+      style: GoogleFonts.roboto().copyWith(color: C.primaryUnHighlightedColor),
+      child: Stack(
+        children: [
           ScreenBackground(elementId: 0),
           Container(
-              child: Column(mainAxisSize: MainAxisSize.min, children: [
-            SizedBox(height: height / 6),
-            Flexible(
-                flex: 4,
-                child: Column(children: [
-                  Container(
-                      child: Image.asset(
-                    S.assetEcellLogoWhite,
-                    width: width * 0.35 * heightFactor,
-                  )),
-                  SizedBox(height: height / 20),
-                  Form(
-                      child: Padding(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: height / 6),
+                Flexible(
+                  flex: 4,
+                  child: Column(
+                    children: [
+                      Container(
+                        child: Image.asset(
+                          S.assetEcellLogoWhite,
+                          width: width * 0.35 * heightFactor,
+                        ),
+                      ),
+                      SizedBox(height: height / 20),
+                      Form(
+                        child: Padding(
                           padding: EdgeInsets.only(
                               left: D.horizontalPadding, right: D.horizontalPadding),
                           child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                _label("First Name", heightFactor),
-                                _labelValue(width, height, user.firstName),
-                                SizedBox(height: height / 30),
-                                _label("Last Name", heightFactor),
-                                _labelValue(width, height, user.lastName),
-                                SizedBox(height: height / 30),
-                                _label("Email Address", heightFactor),
-                                _labelValue(width, height, user.email),
-                                SizedBox(height: height / 30),
-                                _label("Phone Number", heightFactor),
-                                _labelValue(width, height, user.phoneNumber),
-                                SizedBox(height: height / 15),
-                                Container(
-                                    alignment: Alignment.topRight,
-                                    child: GestureDetector(
-                                        onTap: () {}, //TODO
-                                        child: Text("Change Number?",
-                                            style: TextStyle(
-                                              color: C.primaryHighlightedColor,
-                                              fontSize: 20 * heightFactor,
-                                            ))))
-                              ])))
-                ]))
-          ]))
-        ]));
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              _label("First Name", heightFactor),
+                              _labelValue(width, height, user.firstName),
+                              SizedBox(height: height / 30),
+                              _label("Last Name", heightFactor),
+                              _labelValue(width, height, user.lastName),
+                              SizedBox(height: height / 30),
+                              _label("Email Address", heightFactor),
+                              _labelValue(width, height, user.email),
+                              SizedBox(height: height / 30),
+                              _label("Phone Number", heightFactor),
+                              _labelValue(width, height, user.phoneNumber),
+                              SizedBox(height: height / 15),
+                              Container(
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {}, //TODO
+                                  child: Text(
+                                    "Change Password?",
+                                    style: TextStyle(
+                                      color: C.primaryHighlightedColor,
+                                      fontSize: 20 * heightFactor,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   void _profile() {
@@ -104,21 +122,30 @@ class _ProfileScreenState extends State<ProfileScreen> {
   }
 
   Widget _label(String label, double fontSize) {
-    return Text(label,
-        style: TextStyle(
-            color: C.primaryUnHighlightedColor,
-            fontSize: 15 * fontSize,
-            fontWeight: FontWeight.w100,
-            letterSpacing: 1.3));
+    return Text(
+      label,
+      style: TextStyle(
+        color: C.primaryUnHighlightedColor,
+        fontSize: 15 * fontSize,
+        fontWeight: FontWeight.w100,
+        letterSpacing: 1.3,
+      ),
+    );
   }
 
   Widget _labelValue(double width, double height, String value) {
     return Container(
-        width: width,
-        padding: EdgeInsets.only(top: height / 100, bottom: 5),
-        child: Text(value,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, letterSpacing: 1.2)),
-        decoration: BoxDecoration(
-            border: Border(bottom: BorderSide(width: 1, color: C.primaryUnHighlightedColor))));
+      width: width,
+      padding: EdgeInsets.only(top: height / 100, bottom: 5),
+      child: Text(
+        value,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600, letterSpacing: 1.2),
+      ),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(width: 1, color: C.primaryUnHighlightedColor),
+        ),
+      ),
+    );
   }
 }
