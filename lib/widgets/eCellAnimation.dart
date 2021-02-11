d15b8fa36c8893320b684ce3ad1bf7ff177546f3
Hard-Coder05 <smishra1605@gmail.com>
2021-02-11 21:16:34 +0530

    Removed unused imports

--- lib/widgets/eCellAnimation.dart
+++ lib/widgets/eCellAnimation.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:ecellapp/core/res/colors.dart';
 import 'package:ecellapp/core/res/strings.dart';
-import 'package:ecellapp/widgets/screen_background.dart';
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
