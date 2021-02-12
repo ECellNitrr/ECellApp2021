b1e8bf615025908667fb498a25f4700a7a8bbc2f
Hard-Coder05 <smishra1605@gmail.com>
2021-02-12 21:35:10 +0530

    Completed home UI but with hardcoded alignment

--- lib/screens/home/tabs/menu/menu.dart
+++ lib/screens/home/tabs/menu/menu.dart
@@ -1,9 +1,16 @@
 import 'package:ecellapp/core/res/colors.dart';
 import 'package:ecellapp/core/res/dimens.dart';
 import 'package:ecellapp/core/res/strings.dart';
+import 'package:ecellapp/core/utils/injection.dart';
+import 'package:ecellapp/screens/esummit/esummit.dart';
+import 'package:ecellapp/screens/login/cubit/login_cubit.dart';
+import 'package:ecellapp/screens/login/login.dart';
+import 'package:ecellapp/screens/login/login_repository.dart';
 import 'package:ecellapp/widgets/eCellAnimation.dart';
 import 'package:ecellapp/widgets/screen_background.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MenuScreen extends StatefulWidget {
   @override
@@ -15,7 +22,6 @@ class _MenuScreenState extends State<MenuScreen> {
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
     double height = MediaQuery.of(context).size.height;
-    double top = MediaQuery.of(context).viewPadding.top;
     double heightFactor = height / 1000;
     return Scaffold(
       extendBodyBehindAppBar: true,
@@ -25,8 +31,8 @@ class _MenuScreenState extends State<MenuScreen> {
         leading: Container(
           padding: EdgeInsets.only(left: D.horizontalPadding - 10, top: 10),
           child: IconButton(
-            icon: Icon(Icons.menu_rounded, color: Colors.white, size: 40),
-            onPressed: () => Navigator.of(context).pop(),
+            icon: Icon(Icons.power_settings_new_rounded, color: Colors.white, size: 40),
+            onPressed: () => _logout(context),
           ),
         ),
       ),
@@ -35,19 +41,31 @@ class _MenuScreenState extends State<MenuScreen> {
           ScreenBackground(elementId: 0),
           Center(
             child: Container(
-              padding: EdgeInsets.only(bottom: heightFactor * 300),
+              padding: EdgeInsets.only(bottom: heightFactor * 350),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 60,
-                    height: 60,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [C.eSummitButton1, C.eSummitButton2],
+                      ),
+                    ),
+                    constraints: BoxConstraints.tightFor(width: 60, height: 60),
+                    child: GestureDetector(
                       child: Image.asset(
                         S.assetEsummitLogoWhite,
                         width: 30,
+                        height: 30,
+                      ),
+                      onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (_) => ESummitScreen(),
+                        ),
                       ),
                     ),
                   ),
@@ -65,18 +83,24 @@ class _MenuScreenState extends State<MenuScreen> {
           ),
           Center(
             child: Container(
-              padding: EdgeInsets.only(bottom: heightFactor * 75, right: width / 1.5),
+              padding: EdgeInsets.only(bottom: heightFactor * 75, right: width / 1.4),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 60,
-                    height: 60,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [C.bQuizButton1, C.bQuizButton2],
+                      ),
+                    ),
+                    constraints: BoxConstraints.tightFor(width: 60, height: 60),
+                    child: GestureDetector(
                       child:
                           Icon(Icons.search_rounded, size: 30, color: C.primaryUnHighlightedColor),
+                      onTap: () {}, //TODO
                     ),
                   ),
                   SizedBox(height: 10),
@@ -93,17 +117,24 @@ class _MenuScreenState extends State<MenuScreen> {
           ),
           Center(
             child: Container(
-              padding: EdgeInsets.only(bottom: heightFactor * 75, left: width / 1.5),
+              padding: EdgeInsets.only(bottom: heightFactor * 75, left: width / 1.4),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 60,
-                    height: 60,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
-                      child: Icon(Icons.search_rounded), //TODO
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [C.eventsButton1, C.eventsButton2],
+                      ),
+                    ),
+                    constraints: BoxConstraints.tightFor(width: 60, height: 60),
+                    child: GestureDetector(
+                      child: Icon(Icons.search_rounded,
+                          size: 30, color: C.primaryUnHighlightedColor), //TODO
+                      onTap: () {}, //TODO
                     ),
                   ),
                   SizedBox(height: 10),
@@ -120,17 +151,24 @@ class _MenuScreenState extends State<MenuScreen> {
           ),
           Center(
             child: Container(
-              padding: EdgeInsets.only(top: heightFactor * 250, left: width / 2.5),
+              padding: EdgeInsets.only(top: heightFactor * 300, left: width / 2.5),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 60,
-                    height: 60,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
-                      child: Icon(Icons.search_rounded), // TODO
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [C.sponsorsButton1, C.sponsorsButton2],
+                      ),
+                    ),
+                    constraints: BoxConstraints.tightFor(width: 60, height: 60),
+                    child: GestureDetector(
+                      child: Icon(Icons.search_rounded,
+                          size: 30, color: C.primaryUnHighlightedColor), //TODO
+                      onTap: () {}, //TODO
                     ),
                   ),
                   SizedBox(height: 10),
@@ -147,17 +185,23 @@ class _MenuScreenState extends State<MenuScreen> {
           ),
           Center(
             child: Container(
-              padding: EdgeInsets.only(top: heightFactor * 250, right: width / 2.5),
+              padding: EdgeInsets.only(top: heightFactor * 300, right: width / 2.5),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Container(
-                    width: 60,
-                    height: 60,
-                    child: RaisedButton(
-                      onPressed: () {},
-                      shape: CircleBorder(),
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [C.aboutUsButton1, C.aboutUsButton2],
+                      ),
+                    ),
+                    constraints: BoxConstraints.tightFor(width: 60, height: 60),
+                    child: GestureDetector(
                       child: Icon(Icons.group, size: 30, color: C.primaryUnHighlightedColor),
+                      onTap: () {}, //TODO
                     ),
                   ),
                   SizedBox(height: 10),
@@ -173,10 +217,25 @@ class _MenuScreenState extends State<MenuScreen> {
             ),
           ),
           Center(
+              child: Container(
             child: ECellLogoAnimation(),
-          ),
+          )),
         ],
       ),
     );
   }
+}
+
+Future<void> _logout(BuildContext context) async {
+  await sl.get<SharedPreferences>().remove(S.tokenKeySharedPreferences);
+  Scaffold.of(context).showSnackBar(SnackBar(content: Text("Logged Out Successfuly")));
+  Navigator.pushReplacement(
+    context,
+    MaterialPageRoute(
+      builder: (_) => BlocProvider(
+        create: (_) => LoginCubit(APILoginRepository()),
+        child: LoginScreen(),
+      ),
+    ),
+  );
 }
