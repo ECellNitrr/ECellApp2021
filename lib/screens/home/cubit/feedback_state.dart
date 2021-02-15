c4fa924ae36bd4de26854834e5b28914ea58c6f5
Siddharth Mishra <smishra1605@gmail.com>
2021-02-15 10:16:42 +0530

    cubit: ContactUsScreen (#93)

---
+++ lib/screens/home/cubit/feedback_state.dart
@@ -0,0 +1,26 @@
+part of 'feedback_cubit.dart';
+
+abstract class FeedbackState extends Equatable {
+  const FeedbackState();
+  @override
+  List<Object> get props => [];
+}
+
+class FeedbackInitial extends FeedbackState {
+  const FeedbackInitial();
+}
+
+class FeedbackLoading extends FeedbackState {
+  const FeedbackLoading();
+}
+
+class FeedbackSuccess extends FeedbackState {
+  const FeedbackSuccess();
+}
+
+class FeedbackError extends FeedbackState {
+  final String message;
+  const FeedbackError(this.message);
+  @override
+  List<Object> get props => [message];
+}
