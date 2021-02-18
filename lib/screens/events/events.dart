62964974e0b3f5a72ae1846ca7342fe6119a2ca9
treavvasu <write2treav@gmail.com>
2021-02-19 02:27:18 +0530

    bugfix: EventsScreen (#109)
    * moving initial api call to statefulwrapper
    
    * initializing list in repository

--- lib/screens/events/events.dart
+++ lib/screens/events/events.dart
@@ -1,6 +1,7 @@
 import 'package:ecellapp/core/utils/logger.dart';
 import 'package:ecellapp/models/event.dart';
 import 'package:ecellapp/screens/events/cubit/events_cubit.dart';
+import 'package:ecellapp/widgets/stateful_wrapper.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -12,29 +13,30 @@ class EventsScreen extends StatefulWidget {
 class _EventsScreenState extends State<EventsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocConsumer<EventsCubit, EventsState>(
-        listener: (context, state) {
-          if (state is EventsInitial) {
-            _getAllEvents();
-          } else if (state is EventsError) {
-            Scaffold.of(context).showSnackBar(
-              SnackBar(content: Text(state.message)),
-            );
-          }
-        },
-        builder: (context, state) {
-          if (state is EventsInitial) {
-            return _buildInitial();
-          } else if (state is EventsSuccess) {
-            return _buildSuccess(context, state.json);
-          } else if (state is EventsLoading) {
-            return _buildLoading();
-          } else {
-            Log.e(tag: "EventsState", message: "State now is EventsError reached");
-            return Container(); // TODO the user should be shown the error on screen instead of a snackbar, and a retry button.
-          }
-        },
+    return StatefulWrapper(
+      onInit: () => _getAllEvents(),
+      child: Scaffold(
+        body: BlocConsumer<EventsCubit, EventsState>(
+          listener: (context, state) {
+            if (state is EventsError) {
+              Scaffold.of(context).showSnackBar(
+                SnackBar(content: Text(state.message)),
+              );
+            }
+          },
+          builder: (context, state) {
+            if (state is EventsInitial) {
+              return _buildInitial();
+            } else if (state is EventsSuccess) {
+              return _buildSuccess(context, state.json);
+            } else if (state is EventsLoading) {
+              return _buildLoading();
+            } else {
+              Log.e(tag: "EventsState", message: "State now is EventsError reached");
+              return Container(); // TODO the user should be shown the error on screen instead of a snackbar, and a retry button.
+            }
+          },
+        ),
       ),
     );
   }
