fc60590d51d9b0338a5a7106fe3259c71e890418
treav <write2treav@gmail.com>
2021-02-19 08:59:04 +0530

    fix imports

--- lib/screens/splash/splash.dart
+++ lib/screens/splash/splash.dart
@@ -1,19 +1,18 @@
 import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:ecellapp/core/res/dimens.dart';
 import 'package:ecellapp/core/res/strings.dart';
 import 'package:ecellapp/core/utils/injection.dart';
-import 'package:ecellapp/screens/events/cubit/events_cubit.dart';
-import 'package:ecellapp/screens/events/events.dart';
-import 'package:ecellapp/screens/events/events_repository.dart';
 import 'package:ecellapp/screens/home/home.dart';
 import 'package:ecellapp/screens/login/cubit/login_cubit.dart';
 import 'package:ecellapp/screens/login/login.dart';
 import 'package:ecellapp/screens/login/login_repository.dart';
 import 'package:ecellapp/widgets/ecell_animation.dart';
 import 'package:ecellapp/widgets/screen_background.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
