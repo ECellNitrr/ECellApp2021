23ffbac541d456dd7d2421ebda886888b4ffb567
Divy Arpit <arpitdivy@gmail.com>
2021-02-21 14:57:56 +0530

    added aim screen ui

--- lib/screens/about_us/tabs/aim.dart
+++ lib/screens/about_us/tabs/aim.dart
@@ -1,13 +1,202 @@
 import 'package:flutter/cupertino.dart';
+import 'package:ecellapp/core/res/colors.dart';
+import 'package:ecellapp/core/res/dimens.dart';
+import 'package:ecellapp/core/res/strings.dart';
+import 'package:ecellapp/widgets/screen_background.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AimScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO replace this widget with the screen AimScreen
-    return Padding(
-      padding: const EdgeInsets.all(30.0),
-      child: Container(
-        child: Text("Aim"),
+    return Scaffold(
+      body: Stack(
+        children: [
+          ScreenBackground(elementId: 0),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                DefaultTextStyle.merge(
+                  style: GoogleFonts.roboto().copyWith(color: C.primaryUnHighlightedColor),
+                  child: screenBody(context),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget screenBody(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    double height = MediaQuery.of(context).size.height;
+    double bottom = MediaQuery.of(context).viewInsets.bottom;
+    double heightFactor = height / 1000;
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(D.horizontalPadding, 30, D.horizontalPadding, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              backButton(width, height, bottom, heightFactor),
+              aboutUsText(width, height, bottom, heightFactor),
+              aimTextSection(width, height, bottom, heightFactor),
+              visionTextSection(width, height, bottom, heightFactor),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget backButton(double width, double height, double bottom, double heightFactor) {
+    return IconButton(
+      icon: Icon(Icons.arrow_back_ios, color: Colors.white, size: 30),
+      onPressed: () {
+        // to be implemented during screen integration.
+      },
+    );
+  }
+
+  Widget aboutUsText(double width, double height, double bottom, double heightFactor) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(left: width / 50, top: heightFactor * 30),
+      child: Text(
+        "About Us",
+        style: TextStyle(
+          fontSize: 50 * heightFactor,
+          fontWeight: FontWeight.w900,
+          color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  Widget aimTextSection(double width, double height, double bottom, double heightFactor) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(left: width / 50, top: heightFactor * 25),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Aim",
+            style: TextStyle(
+              fontSize: 40 * heightFactor,
+              fontWeight: FontWeight.w900,
+              color: C.primaryHighlightedColor,
+            ),
+          ),
+          SizedBox(
+            height: heightFactor * 10,
+          ),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "At ",
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                TextSpan(
+                  text: "E-Summit ",
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                TextSpan(
+                  text: S.aimPara,
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget visionTextSection(double width, double height, double bottom, double heightFactor) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      padding: EdgeInsets.only(left: width / 50, top: heightFactor * 35),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Vision",
+            style: TextStyle(
+              fontSize: 40 * heightFactor,
+              fontWeight: FontWeight.w900,
+              color: C.primaryHighlightedColor,
+            ),
+          ),
+          SizedBox(
+            height: heightFactor * 10,
+          ),
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "At ",
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+                TextSpan(
+                  text: "E-Summit ",
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  ),
+                ),
+                TextSpan(
+                  text: S.visionPara,
+                  style: TextStyle(
+                    height: 1.5,
+                    letterSpacing: 0.5,
+                    wordSpacing: 1.5,
+                    fontSize: 20 * heightFactor,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
