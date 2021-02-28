a1f558617a7cb9af05941b95d35b5e048032d11f
treav <write2treav@gmail.com>
2021-03-01 00:08:03 +0530

    Staging sponsor UI

--- lib/screens/sponsors/sponsors.dart
+++ lib/screens/sponsors/sponsors.dart
@@ -1,5 +1,4 @@
 import 'package:ecellapp/core/res/colors.dart';
-import 'package:ecellapp/core/res/dimens.dart';
 import 'package:ecellapp/models/sponsor_category.dart';
 import 'package:ecellapp/screens/sponsors/sponsor_card.dart';
 import 'package:ecellapp/widgets/ecell_animation.dart';
@@ -9,6 +8,7 @@ import 'package:ecellapp/widgets/stateful_wrapper.dart';
 import 'package:ecellapp/widgets/vertical_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'cubit/sponsors_cubit.dart';
 
 class SponsorsScreen extends StatelessWidget {
@@ -19,6 +19,19 @@ class SponsorsScreen extends StatelessWidget {
     return StatefulWrapper(
       onInit: () => _getAllSponsors(context),
       child: Scaffold(
+        extendBodyBehindAppBar: true,
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          leading: Container(
+            padding: EdgeInsets.only(left: 10),
+            child: IconButton(
+              icon: Icon(Icons.arrow_back_ios, color: Colors.black, size: 30),
+              onPressed: () => Navigator.of(context).pop(),
+            ),
+          ),
+        ),
         body: Stack(
           children: [
             ScreenBackground(elementId: 0),
@@ -41,22 +54,20 @@ class SponsorsScreen extends StatelessWidget {
   }
 
   Widget _buildSuccess(BuildContext context, List<SponsorCategory> sponsorsList) {
-    double height = MediaQuery.of(context).size.height;
-    double heightFactor = height / 1000;
-    double bottom = MediaQuery.of(context).viewInsets.bottom;
-
-    //TODO: UI
+    double top = MediaQuery.of(context).viewPadding.top;
     List<Widget> sL = [];
-    sL.add(Center(
-      child: Text(
-        "Sponsors",
-        style: TextStyle(
-          fontSize: 40,
-          color: Colors.white,
-          fontWeight: FontWeight.w800,
-          letterSpacing: 0.5,
-        ),
-      ),
+    sL.add(Column(
+      children: [
+        SizedBox(height: 20),
+        Center(
+            child: Text("Sponsors",
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w800,
+                  letterSpacing: 0.5,
+                )))
+      ],
     ));
     for (var item in sponsorsList) {
       if (item.category.contains('T')) {
@@ -66,87 +77,48 @@ class SponsorsScreen extends StatelessWidget {
       }
     }
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      backgroundColor: Colors.transparent,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        leading: Container(
-          padding: EdgeInsets.only(left: 10),
-          child: IconButton(
-            icon: Icon(Icons.arrow_back_ios, color: Colors.black, size: 30),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-        ),
-      ),
-      body: Container(
-        color: Colors.white,
-        child: Row(
-          children: [
-            Expanded(
-              flex: 1,
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: [
-                  Expanded(flex: 2, child: Container()),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: VerticalText(
-                      checked: false,
-                      name: "Title",
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: VerticalText(
-                      checked: false,
-                      name: "Associate",
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: VerticalText(
-                      checked: false,
-                      name: "Platinum",
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: VerticalText(
-                      checked: false,
-                      name: "Gold",
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.tight,
-                    child: VerticalText(
-                      checked: false,
-                      name: "Partner",
-                    ),
-                  ),
-                  Expanded(flex: 2, child: Container()),
-                ],
+    List<String> tabs = ["Title", "Associates", "Platinum", "Gold", "Partner"];
+    List<Widget> tabNames = [SizedBox(height: 150)];
+
+    tabs.forEach((element) {
+      tabNames.add(
+        Flexible(flex: 1, fit: FlexFit.loose, child: VerticalText(checked: false, name: element)),
+      );
+    });
+
+    return DefaultTextStyle.merge(
+      style: GoogleFonts.roboto().copyWith(color: C.primaryUnHighlightedColor),
+      child: NotificationListener<OverscrollIndicatorNotification>(
+        onNotification: (OverscrollIndicatorNotification overscroll) {
+          overscroll.disallowGlow();
+          return true;
+        },
+        child: Container(
+          color: Colors.white,
+          child: Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: tabNames,
+                ),
               ),
-            ),
-            Expanded(
-                flex: 9,
-                child: Container(
-                  child: Column(
-                    children: sL,
-                    mainAxisSize: MainAxisSize.max,
-                  ),
-                  decoration: BoxDecoration(
-                      color: Colors.deepPurpleAccent,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(40), topLeft: Radius.circular(40))),
-                ))
-          ],
+              Expanded(
+                  flex: 9,
+                  child: Container(
+                    child: Column(
+                      children: sL,
+                      mainAxisSize: MainAxisSize.max,
+                    ),
+                    decoration: BoxDecoration(
+                        color: Colors.deepPurpleAccent,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(40), topLeft: Radius.circular(40))),
+                  ))
+            ],
+          ),
         ),
       ),
     );
