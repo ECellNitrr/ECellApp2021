7c358fdc76c28896e330d96ee9e8555f30ab172c
treav <write2treav@gmail.com>
2021-03-14 00:11:39 +0530

    check fix

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,7 @@ class ECellApp extends StatelessWidget {
           S.routeAboutUs: (_) =>
               BlocProvider(create: (_) => TeamCubit(FakeTeamRepository()), child: AboutUsScreen()),
         },
-        initialRoute: S.routeHome ,
+        initialRoute: S.routeSplash,
         title: "ECellApp",
         theme: AppTheme.themeData(context),
       ),
